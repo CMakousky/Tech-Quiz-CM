1849f2eca21dc2d5f94b889d5aeb7c823b9feba2
Christopher Makousky <christopher.makousky@gmail.com>
2025-01-30 09:17:44 -0600

    End-to-end testing now incorporates mock data from cypress/fixtures/questions.json.

--- cypress/e2e/quiz.cy.ts
+++ cypress/e2e/quiz.cy.ts
@@ -1,16 +1,14 @@
-const quizUri = "http://127.0.0.1:3001/"
-
 describe('tech quiz', () => {
   context('Setup', () => {
     it('should render the page and present a "Start Quiz" button to the user', () => {
-      cy.visit(`${quizUri}`);
+      cy.visit('/');
       cy.get('[data-cy=start-screen]').should('be.visible');
     });
   });
 
   context('Start Quiz', () => {
     it('should progess to the quiz when the "Start Quiz" button is clicked', () => {
-      cy.visit(`${quizUri}`);
+      cy.visit('/');
       cy.get('[data-cy=start-button]').click();
       cy.get('[data-cy=quiz-question]').should('be.visible');
     });
@@ -18,7 +16,7 @@ describe('tech quiz', () => {
 
   context('Answer Questions', () => {
     it('should display the score once 10 questions are answered', () => {
-      cy.visit(`${quizUri}`);
+      cy.visit('/');
       cy.get('[data-cy=start-button]').click();
       for (let question = 0; question < 10; question++) {
         cy.get('[data-cy=button-0]').click();
@@ -27,9 +25,55 @@ describe('tech quiz', () => {
     });
   });
 
+  context('Answer Questions Correctly', () => {
+    beforeEach(() => {
+      cy.intercept({
+        method: 'GET',
+        url: '/api/questions/random'
+      },
+      {
+        fixture: 'questions.json',
+        statusCode: 200
+      }
+      ).as('fixtureQuestions');
+      cy.visit('/');
+    });
+
+    it('should display the score of 10/10 once 10 questions are answered correctly', () => {
+      cy.get('[data-cy=start-button]').click();
+      for (let question = 0; question < 10; question++) {
+        cy.get('[data-cy=button-0]').click();
+      };
+      cy.get('[data-cy=completion-screen]').should('contain', 'Your score: 10/10');
+    });
+  });
+
+  context('Answer Questions Incorrectly', () => {
+    beforeEach(() => {
+      cy.intercept({
+        method: 'GET',
+        url: '/api/questions/random'
+      },
+      {
+        fixture: 'questions.json',
+        statusCode: 200
+      }
+      ).as('fixtureQuestions');
+      cy.visit('/');
+    });
+
+    it('should display the score of 0/10 once 10 questions are answered incorrectly', () => {
+      cy.get('[data-cy=start-button]').click();
+      for (let question = 0; question < 10; question++) {
+        cy.get('[data-cy=button-1]').click();
+      };
+      cy.get('[data-cy=completion-screen]').should('contain', 'Your score: 0/10');
+    });
+  });
+
   context('Start a New Quiz', () => {
     it ('should take the user to a new quiz when the "Take New Quiz" button is pressed at the end of a quiz', () => {
-      cy.visit(`${quizUri}`);
+      cy.visit('/');
       cy.get('[data-cy=start-button]').click();
       for (let question = 0; question < 10; question++) {
         cy.get('[data-cy=button-0]').click();
@@ -38,4 +82,4 @@ describe('tech quiz', () => {
       cy.get('[data-cy=quiz-question]').should('be.visible');
     });
   });
-})
+});
