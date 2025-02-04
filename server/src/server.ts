d7e1300f4f3f9885ea4cf0c7cdba5b99f05b1140
Christopher Makousky <christopher.makousky@gmail.com>
2025-02-04 13:57:10 -0600

    Production mode serves files correctly now.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,12 @@
+// import type { Request, Response } from 'express';
 import express from 'express';
 import path from 'node:path';
+import db from './config/connection.js';
+import routes from './routes/index.js';
 
 import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-import db from './config/connection.js';
-import routes from './routes/index.js';
 
 await db();
 
@@ -15,15 +15,16 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
 
-   app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-}
+  // app.get('*', (_req: Request, res: Response) => {
+  //   res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+  // });
+};
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`API server running on port ${PORT}!`);
