c0782f9ca573413bccf830324055bf4ffab8131d
Christopher Makousky <christopher.makousky@gmail.com>
2025-02-05 21:16:42 -0600

    Removed dead code from server/src/server/ts. Changed line 1 of .npmrc from 'production=true' to 'production=false'.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,3 @@
-// import type { Request, Response } from 'express';
 import express from 'express';
 import path from 'node:path';
 import db from './config/connection.js';
@@ -18,10 +17,6 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../client/dist')));
-
-  // app.get('*', (_req: Request, res: Response) => {
-  //   res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
-  // });
 };
 
 app.use(routes);
